13e77a6e9dee3643114e7aafb37f4239b2e1b36f
MathieuBo <mbourdenx@me.com>
2022-07-08 10:32:23 +0100

    new dataset

---
+++ src/config.js
@@ -0,0 +1,16 @@
+function config() {
+    var ini = [
+        {   // 1.
+            name: 'default',
+            img_width: 10956,
+            img_height: 10691,
+            img_depth: 68 * 3.8462 * 0.9,
+            particle_size: 8000.0,
+            // zThres: 1500.0,
+            geneData: 'https://api.github.com/repos/acycliq/B2A3/contents/data/geneData?ref=master',
+            cellData: 'https://api.github.com/repos/acycliq/B2A3/contents/data/cellData?ref=master',
+        },
+    ];
+    return d3.map(ini, function (d) {return d.name;})
+}
+
